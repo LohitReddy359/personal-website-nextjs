b41d85e060915894427270d5ff47536de5bdd23e
LohitReddy359 <kamatham.lohit@gmail.com>
2022-07-23 22:09:05 -0400

    HomePageText Component

---
+++ components/home_page/IntroText.js
@@ -0,0 +1,18 @@
+function IntroText(props) {
+  return (
+    <div>
+      <h1 className="name">
+        Hey,
+        <br />
+        I'm Lohit
+      </h1>
+      <h1 className="career">
+        University of Michigan, Ann Arbor
+        <br />
+        Computer Science | Economics
+      </h1>
+    </div>
+  );
+}
+
+export default IntroText;
