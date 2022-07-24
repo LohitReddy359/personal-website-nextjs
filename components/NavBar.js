4721f3027fe5d6423d1a213677b6060c654e3f87
LohitReddy359 <kamatham.lohit@gmail.com>
2022-07-23 23:32:42 -0400

    Added Navigation Bar

---
+++ components/NavBar.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../assets/Logo.png";
+
+function NavBar() {
+  return (
+    <div className="nav-container">
+      <nav>
+        <div className="logo">
+          <Image src={logo} width={50} height={50} />
+        </div>
+        <ul className="nav-items">
+          <li>
+            <Link href="#">Experience</Link>
+          </li>
+          <li>
+            <Link href="#">Projects</Link>
+          </li>
+          <li>
+            <Link href="#">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
