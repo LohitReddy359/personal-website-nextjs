829a1b722c5c2428f5debce19491b5b518833237
LohitReddy359 <kamatham.lohit@gmail.com>
2022-08-08 14:04:12 -0400

    Added a section heading component for every section to use

---
+++ components/SectionHeading.js
@@ -0,0 +1,13 @@
+
+
+function SectionHeading(props) {
+    return(
+        <div>
+            <h1 className = "section-heading">
+                {props.text}
+            </h1>
+        </div>
+    );
+}
+
+export default SectionHeading;
