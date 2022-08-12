362861e59ea5cebdd111ff181f9a276b5423bad3
LohitReddy359 <kamatham.lohit@gmail.com>
2022-08-11 21:11:59 -0400

    Styled Nav Bar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,29 +1,32 @@
 import Link from "next/link";
-import Image from "next/image";
-import logo from "../assets/Logo.png";
+
+import styles from "./navbar.module.css";
 
 function NavBar() {
   return (
-    <div className="nav-container">
-      <nav>
-        <div className="logo">
-          <Image src={logo} width={50} height={50} />
-        </div>
-        <ul className="nav-items">
-          <li>
-            <Link href="about">About</Link>
-          </li>
-          <li>
-            <Link href="experience">Experience</Link>
-          </li>
-          <li>
-            <Link href="projects">Projects</Link>
-          </li>
-          <li>
-            <Link href="contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className={styles.navcontainer}>
+      <ul className={styles.navitems}>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/experience">
+            <a>Experience</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/projects">
+            <a>Projects</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
