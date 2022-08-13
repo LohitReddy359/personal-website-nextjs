0a19984cd12acbd9d654900ce3502b96a1e6116f
LohitReddy359 <kamatham.lohit@gmail.com>
2022-08-13 00:09:48 -0400

    Added NavBar bg image and styled the items (added logo to go back to home page)

--- components/layout/NavBar.js
+++ components/layout/NavBar.js
@@ -1,11 +1,20 @@
 import Link from "next/link";
-
+import Image from "next/Image";
 import styles from "./navbar.module.css";
+
+import Logo from "../../assets/Logo.png";
 
 function NavBar() {
   return (
     <div className={styles.navcontainer}>
       <ul className={styles.navitems}>
+        <li className={styles.logo}>
+          <Link href="/#">
+            <div className = {styles.logoimg}>
+            <Image src={Logo} alt="Logo" />
+            </div>
+          </Link>
+        </li>
         <li>
           <Link href="/about">
             <a>About</a>
