1fd5ff796a8c30fca6297dfa77d8b9e566c4726a
LohitReddy359 <kamatham.lohit@gmail.com>
2022-08-14 21:40:33 -0400

    Modified Contact Page and styled the content

--- components/contact/Contact.js
+++ components/contact/Contact.js
@@ -1,78 +1,83 @@
 import Link from "next/link";
 
+import styles from './contact.module.css'
+
 function Contact() {
   return (
-    <div>
-      <section>
-        <form method="post" action="#">
-          <div class="fields">
-            <div class="field">
-              <label for="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div class="field">
-              <label for="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div class="field">
-              <label for="message">Message</label>
-              <textarea name="message" id="message" rows="3"></textarea>
-            </div>
-          </div>
-          <ul class="actions">
-            <li>
-              <input type="submit" value="Send Message" />
-            </li>
-          </ul>
-        </form>
-      </section>
-      <section class="split contact">
-        <section class="alt">
-          <h3>Address</h3>
-          <p>
-            525 Walnut St. #05
-            <br />
-            Ann Arbor, MI-48104
-          </p>
-        </section>
+    <div className = {styles.footer}>
         <section>
-          <h3>Phone</h3>
-          <p>(734) 510-0789</p>
+          <form method="post" action="#" className = {styles.formf}>
+            <div className={styles.fields}>
+              <div className={styles.field}>
+                <label for="name" className = {styles.labell}>Name </label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className={styles.field}>
+                <label for="email" className = {styles.labell}>Email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className={styles.field}>
+                <label for="message" className = {styles.labell}>Message</label>
+                <textarea name="message" id="message" rows="3"></textarea>
+              </div>
+            </div>
+            <ul className={styles.action}>
+              <li>
+                <input type="submit" value="Send Message" className = {styles.buttonsubmit}/>
+              </li>
+            </ul>
+          </form>
         </section>
-        <section>
-          <h3>Email</h3>
-          <p>
-            <Link href="mailto:kamatham.lohit@gmail.com">
-              kamatham.lohit@gmail.com
-            </Link>
-          </p>
+        <section className={styles.splitcontact}>
+          <section className={styles.alt}>
+            <h3>Address</h3>
+            <p>
+              {" "}
+              525 Walnut St. #05
+              <br />
+              Ann Arbor, MI-48104
+            </p>
+          </section>
+          <section>
+            <h3>Phone</h3>
+            <p>(734) 510-0789</p>
+          </section>
+          <section>
+            <h3>Email</h3>
+            <p>
+              <Link href="mailto:kamatham.lohit@gmail.com">
+                <a>
+                kamatham.lohit@gmail.com
+                </a>
+              </Link>
+            </p>
+          </section>
+          <section>
+            <h3>Social</h3>
+            <ul className={styles.iconsalt}>
+              <li>
+                <a href="https://www.linkedin.com/in/lohit-kamatham-52b183226/" className={styles.icon}>
+                  <span className={styles.label}>Linkedin</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/LohitReddy359" className={styles.icon}>
+                  <span className={styles.label}>Github</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/lohit_reddyk/" className={styles.icon}>
+                  <span className={styles.label}>Instagram</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.snapchat.com/add/lohitreddy359?share_id=59_YEeLu2BM&locale=en-US" className={styles.icon}>
+                  <span className={styles.label}>Snapchat</span>
+                </a>
+              </li>
+            </ul>
+          </section>
         </section>
-        <section>
-          <h3>Social</h3>
-          <ul class="icons alt">
-            <li>
-              <a href="#" class="icon brands alt fa-twitter">
-                <span class="label">Twitter</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="icon brands alt fa-facebook-f">
-                <span class="label">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="icon brands alt fa-instagram">
-                <span class="label">Instagram</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="icon brands alt fa-github">
-                <span class="label">GitHub</span>
-              </a>
-            </li>
-          </ul>
-        </section>
-      </section>
     </div>
   );
 }
