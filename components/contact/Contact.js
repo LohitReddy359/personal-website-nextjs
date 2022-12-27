f2efbf8ca7bd5fb0d92d147fc4de07bf38db059a
LohitReddy359 <kamatham.lohit@gmail.com>
2022-12-27 14:02:15 -0500

    Testing Fix for Contact Page

--- components/contact/Contact.js
+++ components/contact/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
   return (
     <div className={styles.footer}>
       <section>
-        <form method="post" action="" className={styles.formf}>
+        <form method="post" action="https://lohit.vercel.app" className={styles.formf}>
           <div className={styles.fields}>
             <div className={styles.field}>
               <label for="name" className={styles.labell}>
@@ -31,7 +31,7 @@ function Contact() {
             <li>
               <input
                 type="submit"
-                value="No Send Message"
+                value="Send Message"
                 className={styles.buttonsubmit}
               />
             </li>
