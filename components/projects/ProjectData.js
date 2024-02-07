06ac45fcaa6e02185021f5dccc76e3c4d2da5c09
Lohit Kamatham <kamatham.lohit@gmail.com>
2024-02-07 11:05:40 -0500

    Included Zelda 494 Project

--- components/projects/ProjectData.js
+++ components/projects/ProjectData.js
@@ -28,4 +28,14 @@ export const projects_data = [
       "Designed and created an online attendance tracker for the company InsemiTechnologies along with a team of 4 interns in order to monitor the attendance of employees during any work from home period",
     link: "https://github.com/arnyboy101/InsemiTimeSheet",
   },
+  {
+    id: 4,
+    title: "Zelda Link Game",
+    date: "Jan 2024 - Feb 2024",
+    tech: "Unity, C#, Git",
+    description:
+      "Designed and implemented a unique twist to Zelda NES. I implemented the weapons for link and the custom mechanic + custom level of the lava dungeon (press key 4). I also implemented the player knockback and old man room",
+    link: "https://nithisha.itch.io/the-legend-of-zelda-nes",
+  },
+  
 ];
