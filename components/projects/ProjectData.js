3ef7318b429a8ffe2dd20b65e6163fa79b207147
Lohit Kamatham <kamatham.lohit@gmail.com>
2024-04-21 17:23:41 -0400

    Update ProjectData.js
    Added RoboZoo

--- components/projects/ProjectData.js
+++ components/projects/ProjectData.js
@@ -3,6 +3,33 @@
 export const projects_data = [
   {
     id: 1,
+    title: "RoboZoo: Laid Off",
+    date: "Mar 2024 - April 2024",
+    tech: "Unity, C#, Git",
+    description:
+      "3D Adventure-Puzzle Character Switching Game where attend a corporate test against AI to secure a job",
+    link: "https://lohitk1.itch.io/gravity-gun",
+  },  
+  {
+    id: 1,
+    title: "Gravity Gun",
+    date: "Feb 2024 - Mar 2024",
+    tech: "Unity, C#, Git",
+    description:
+      "2D Puzzle Game",
+    link: "https://lohitk1.itch.io/gravity-gun",
+  },
+  {
+    id: 2,
+    title: "Zelda Link Game",
+    date: "Jan 2024 - Feb 2024",
+    tech: "Unity, C#, Git",
+    description:
+      "Designed and implemented a unique twist to Zelda NES. I implemented the weapons for link and the custom mechanic + custom level of the lava dungeon (press key 4). I also implemented the player knockback and old man room",
+    link: "https://nithisha.itch.io/the-legend-of-zelda-nes",
+  },
+  {
+    id: 3,
     title: "Personal Website",
     date: "July 2022 - Aug 2022",
     tech: "NextJS (React Framework), HTML, CSS, SASS, Figma",
@@ -11,31 +38,14 @@ export const projects_data = [
     link: "https://github.com/LohitReddy359/personal-website-nextjs",
   },
   {
-    id: 2,
+    id: 4,
     title: "Real-Time Messaging",
     date: "July 2022 - Aug 2022",
     tech: "PusherAPI, React, Django",
     description:
       "Implemented Real-Time messaging for the startup nSpireAI's mobile/web applications which allowed users to send and receive messages in real time",
     link: "N/A",
-  },
-  {
-    id: 3,
-    title: "gravity Gun",
-    date: "",
-    tech: "Unity",
-    description:
-      "Entire game made by me",
-    link: "https://lohitk1.itch.io/gravity-gun",
-  },
-  {
-    id: 4,
-    title: "Zelda Link Game",
-    date: "Jan 2024 - Feb 2024",
-    tech: "Unity, C#, Git",
-    description:
-      "Designed and implemented a unique twist to Zelda NES. I implemented the weapons for link and the custom mechanic + custom level of the lava dungeon (press key 4). I also implemented the player knockback and old man room",
-    link: "https://nithisha.itch.io/the-legend-of-zelda-nes",
-  },
+  }
+  
   
 ];
